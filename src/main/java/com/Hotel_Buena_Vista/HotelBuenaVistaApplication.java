8aa78dc37edaad32a697272b67a6bb8a1513dbb0
celeste cerdas <128451886+celeste1107@users.noreply.github.com>
2026-03-07 16:41:14 -0600

    inicio
    inicio 1

---
+++ src/main/java/com/Hotel_Buena_Vista/HotelBuenaVistaApplication.java
@@ -0,0 +1,13 @@
+package com.Hotel_Buena_Vista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBuenaVistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBuenaVistaApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/Hotel_Buena_Vista/ProjectConfig.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Hotel_Buena_Vista;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+@Configuration
+public class ProjectConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/principal").setViewName("principal");
+        registry.addViewController("/inicioSeccion").setViewName("inicioSeccion");
+        registry.addViewController("/registro").setViewName("registro");
+        
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver_0() {
+        SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+        resolver.setPrefix("classpath:/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setOrder(0);
+        resolver.setCheckExistence(true);
+        return resolver;
+    }
+ }
+    

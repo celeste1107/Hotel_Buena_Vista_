775a26ea9f782c5ed0d53734cbb44fe53689e399
celeste cerdas <128451886+celeste1107@users.noreply.github.com>
2026-04-04 22:05:57 -0600

    trabajando
    en H13

--- src/main/java/com/Hotel_Buena_Vista/HotelBuenaVistaApplication.java
+++ src/main/java/com/Hotel_Buena_Vista/HotelBuenaVistaApplication.java
@@ -3,6 +3,8 @@ package com.Hotel_Buena_Vista;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class HotelBuenaVistaApplication {
 
